c61c3d86a0a8e1a38e06db7972f486bfcca255b4
Deepak Kohli <deepakkohli1810@gmail.com>
2025-01-10 19:48:46 +0530

    New Browser Router

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter, Route } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
-    <App />
-    </HashRouter>
+    <BrowserRouter basename='/tourist-hero-ultimate/'>
+   
+      <App/>
+    
+    </BrowserRouter>
   </StrictMode>,
 )
