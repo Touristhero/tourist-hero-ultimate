ab216ffa74046b9cf079cf9c31deca62be6b9e74
Deepak Kohli <deepakkohli1810@gmail.com>
2025-01-10 16:56:54 +0530

    Made changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/tourist-hero-ultimate',
+  base:'/tourist-hero-ultimate/',
    build: {
     outDir: 'build'
   },
